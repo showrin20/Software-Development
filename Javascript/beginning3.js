fd57e7379d74f7d871fa2f1479f63102a81c3226
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-03-23 13:58:44 +0600

    Practice_3

---
+++ Javascript/beginning3.js
@@ -0,0 +1,33 @@
+// Coding challenge #36. Create a function that converts a string to an array of characters
+
+
+// Coding challenge #37. Create a function that will convert a string in an array containing the ASCII codes of each character
+
+// Coding challenge #38. Create a function that will convert an array containing ASCII codes in a string
+
+// Coding challenge #39. Implement the Caesar cypher
+// Decrypt a message by using the same encrypt function
+// ... but using the inverse of the key (e.g. rotate in the other direction)
+// Function will implement Caesar Cipher to
+// encrypt / decrypt the msg by shifting the letters
+// of the message acording to the key
+// Modulo function: n mod p
+
+
+
+// Coding challenge #40. Implement the bubble sort algorithm for an array of numbers
+
+
+// Coding challenge #41. Create a function to calculate the distance between two points defined by their x, y coordinates
+
+
+// Coding challenge #42. Create a function that will return a Boolean value indicating if two circles defined by center coordinates and radius are intersecting
+// Calculate the distance between the two specified points
+
+
+// Coding challenge 43. Create a function that will receive a bi-dimensional array as argument and a number and will extract as a unidimensional array the column specified by the number
+
+// Coding challenge #44. Create a function that will convert a string containing a binary number into a number
+
+
+// Coding challenge #45. Create a function to calculate the sum of all the numbers in a jagged array (array contains numbers or other arrays of numbers on an unlimited number of levels)
