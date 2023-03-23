69698db37bb49e34f8dc460f14a9ad280a9c3056
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-03-23 13:57:30 +0600

    Practice 2

---
+++ Javascript/beginning2.js
@@ -0,0 +1,31 @@
+// Coding challenge #25: Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first array or second array but not in both
+
+
+
+
+// Coding challenge #26: Create a function that will receive two arrays and will return an array with elements that are in the first array but not in the second 
+
+
+
+// Coding challenge #27: Create a function that will receive an array of numbers as argument and will return a new array with distinct elements (no duplicates)
+
+// Coding challenge #30-a: Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should be also provided as string.
+
+// Coding challenge #53. Calculate 70! with high precision (all digits)
+
+
+
+// Coding challenge #31a. Create a function that will return the number of words in a text
+
+// Coding challenge #32. Create a function that will capitalize the first letter of each word in a text
+
+
+// Coding challenge #33. Calculate the sum of numbers received in a comma delimited string
+
+
+// Coding challenge #34. Create a function that will return an array with words inside a text
+
+
+// Coding challenge #14: Print the first 10 Fibonacci numbers without recursion
+// Coding challenge #15: Create a function that will find the nth Fibonacci number using recursion
+// Coding challenge #52. Calculate Fibonacci(500) with high precision (all decimals)
