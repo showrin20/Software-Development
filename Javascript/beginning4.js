013a5207953268e2f98af5c7a1bd75c5e1939eff
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2023-03-23 13:59:17 +0600

    Practice 4

---
+++ Javascript/beginning4.js
@@ -0,0 +1,20 @@
+// Coding challenge #46-a. Find the maximum number in a jagged array of numbers or array of numbers
+// Use recursion to find the maximum numeric value in an array of arrays
+    // Cycle through all the elements of the array
+        // If an element is of type array then invoke the same function
+        // to find out the maximum element of that subarray
+
+// Coding challenge #47. Deep copy a jagged array with numbers or other arrays in a new array
+
+
+// Coding challenge #48. Create a function to return the longest word(s) in a string
+// Get only the elements from specified positions from the array
+// Returns an array with the words from specified text
+
+
+
+
+// Coding challenge #49. Shuffle an array of strings
+// Shuffle array implemented using Fisher–Yates shuffle algorithm
+// Get a random int between min and max (both included)
+
